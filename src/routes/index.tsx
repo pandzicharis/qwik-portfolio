a08e43193a7222c4c1dc6da88527752c0de5ee23
Haris Pandzic <pandzicharis@hotmail.com>
2023-06-10 20:38:10 +0200

    Qwik setup.

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <p>Welcome to my portfolio!</p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
